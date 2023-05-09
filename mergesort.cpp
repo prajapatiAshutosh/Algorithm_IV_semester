2dfaa29968ced797d6c12557d50eb5ce9c32d901
ASHUTOSH PRAJAPATI <114522500+prajapatiAshutosh@users.noreply.github.com>
2023-05-09 14:09:11 +0530

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 void inputarray(int ar[],int n)
 {
-	for(int i=0;i<n;i++)
-	{
-		cin>>ar[i];
-	}
+    for(int i=0;i<n;i++)
+    {
+        cin>>ar[i];
+    }
 }
 
-void merging(int ar[],int l,int m,int r)
+void merging(int ar[],int l,int m,int r, int &count)
 {
-	int n1 = m - l + 1;
+    int n1 = m - l + 1;
     int n2 = r - m;
  
     int L[n1], R[n2];
@@ -26,6 +26,7 @@ void merging(int ar[],int l,int m,int r)
     int k = l;
  
     while (i < n1 && j < n2) {
+        count++; // increment count for comparison operation
         if (L[i] <= R[j]) {
             ar[k] = L[i];
             i++;
@@ -50,41 +51,42 @@ void merging(int ar[],int l,int m,int r)
     }
 }
 
-void mergesort(int ar[],int l,int r)
+void mergesort(int ar[],int l,int r, int &count)
 {
-	if(l<r)
-	{
-		int mid = l+(r-l)/2;
-		mergesort(ar,l,mid);
-		mergesort(ar,mid+1,r);	
-		merging(ar,l,mid,r);
-	}
-	return;
+    if(l<r)
+    {
+        int mid = l+(r-l)/2;
+        mergesort(ar,l,mid, count);
+        mergesort(ar,mid+1,r, count);    
+        merging(ar,l,mid,r, count);
+    }
+    return;
 }
 
 void printarray(int ar[],int n)
 {
-	cout<<endl;
-	for(int i=0;i<n;i++)
-	{
-		cout<<ar[i]<<" ";
-	}
-	cout<<endl;
+    cout<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<ar[i]<<" ";
+    }
+    cout<<endl;
 }
 
 int main()
 {
-	int n;
-	int *ar;
-	cout<<"Enter the size of the array: ";
-	cin>>n;
-	ar = new int[n];
-	cout<<"Enter the data of the array: ";
-	inputarray(ar,n);
-	cout<<"The entered array is: ";
-	printarray(ar,n);
-	mergesort(ar,0,n-1);
-	cout<<"The Sorted array is: ";
-	printarray(ar,n);
-	
+    int n;
+    int *ar;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    ar = new int[n];
+    cout<<"Enter the data of the array: ";
+    inputarray(ar,n);
+    cout<<"The entered array is: ";
+    printarray(ar,n);
+    int count = 0; // initialize count to zero
+    mergesort(ar,0,n-1, count);
+    cout<<"The Sorted array is: ";
+    printarray(ar,n);
+    cout<<"The number of comparisons performed: "<<count<<endl; // print the number of comparisons performed
 }
